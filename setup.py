8c2a46aded13e39035d319f53c1daf464b9b7d46
Chris Doherty <chris.doherty4@gmail.com>
2020-06-28 17:50:22 -0600

    Update setup.py with description and classifiers

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
 from setuptools import setup, find_packages
 
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="Case Converter",
-    version="1.0",
+    version="1.0.0",
     url="https://github.com/chrisdoherty4/python-case-converter",
     packages=find_packages(exclude=["*_test.py"]),
     author="Chris Doherty",
     author_email="chris@chrisdoherty.io",
-    description="Case conversion package for strings.",
-    keywords="case convert converter string",
+    description="A string case conversion package.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=["case", "convert", "converter", "string"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Topic :: Utilities",
+        "Topic :: Text Processing",
+    ],
 )
